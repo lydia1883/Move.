bc7d00a468b517e0817df35771a018abac8dbfb8
EVANS <986981086@qq.com>
2015-12-04 13:17:12 +0800

    move.js
    move

---
+++ move.js
@@ -0,0 +1,62 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2015-12-04 12:29:05
+ * @version $Id$
+ */
+
+
+function getStyle(obj,attr){
+
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	
+	}else{
+		return getComputedStyle(obj,false)[attr];
+	}
+
+}
+
+  function move(obj,json,fnEnd){
+
+  	  clearInterval(obj.timer);
+
+
+  	  obj.timer=setInterval(function(){
+  	  	
+  	  	var bStop=true;
+  	  	for(var attr in json){
+
+  	  				var cur=0;
+
+		  	  		if(attr=='opacity'){
+		  	  			cur=Math.round(parseFloat(getStyle(obj,attr))*100);
+		  	  		}else{
+
+		  	  			cur=parseInt(getStyle(obj,attr));
+		  	  		}
+		  	  		var speed=(json[attr]-cur)/6;
+		  	  		speed=speed>0?Math.ceil(speed):Math.floor(speed);
+
+	  	  			if(cur!=json[attr]){
+	  	  				bStop=false;
+	  	  			}	
+	  	  	     	if(attr=='opacity'){
+	  	  				obj.style.filter='alpha(opacity:'+(cur+speed)+')';
+	  	  				obj.style.opacity=(cur+speed)/100;
+	  	   			}
+	  	   			else{
+	  	   				obj.style[attr]=cur+speed+'px';
+	  	   			}	
+	  	   			
+		  	  	}
+  		
+				if(bStop){
+	  	  			clearInterval(obj.timer);
+	  	  			if(fnEnd)fnEnd();
+	  	  	   	}
+
+  	  },30);
+  }
+  
+
